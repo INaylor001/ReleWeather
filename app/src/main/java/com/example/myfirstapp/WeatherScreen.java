b565949d8b574104da975557b032e632f68429d4
Irvin Naylor <Inaylor003@gmail.com>
2020-05-03 21:27:31 -0400

    Changes: - added clothing suggestion screen with values - added pop up when use selects the figure for reccomendations

--- app/src/main/java/com/example/myfirstapp/WeatherScreen.java
+++ app/src/main/java/com/example/myfirstapp/WeatherScreen.java
@@ -2,15 +2,35 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class WeatherScreen extends AppCompatActivity {
+
+    ImageButton myImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_screen);
 
+        //code for stickman image click
+
+        myImageButton = findViewById(R.id.stickman);
+
+        myImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), PopActivity.class);
+                startActivity(i);
+            }
+        });
+                                         }
+
+
         //mostly WIP for now
     }
-}
+
